3942c9d39a3e02d5c438249ea0e0ee708d85516a
yberkharmansah <yberkharmansah@gmail.com>
2025-05-31 23:56:16 +0300

    type error fix

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
       <div class="container-fluid">
         <router-link to="/" class="navbar-brand fs-4 fw-bold text-white hover:text-info">
-          Yasin Berk Harmansah
+          Yasar Berk Harmansah
         </router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
